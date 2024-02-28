37b2b317268dbd5e2d7aac527633cc34b821c256
joseManuel1234 <josemrodriguez@javeriana.edu.co>
2024-02-27 22:25:04 -0500

    feature:pre-merge-commit

---
+++ app/src/main/java/com/example/loginactivity/FotosRecientesActivity.kt
@@ -0,0 +1,11 @@
+package com.example.loginactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FotosRecientesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fotos_recientes)
+    }
+}
--- app/src/main/java/com/example/loginactivity/OrientacionSexualActivity.kt
+++ app/src/main/java/com/example/loginactivity/OrientacionSexualActivity.kt
@@ -2,10 +2,53 @@ package com.example.loginactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CheckBox
+import android.widget.TableLayout
+import android.widget.TableRow
+import com.example.loginactivity.databinding.ActivityOrientacionSexualBinding
 
 class OrientacionSexualActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOrientacionSexualBinding
+    private val selectedOptions = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_orientacion_sexual)
+        binding = ActivityOrientacionSexualBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val opciones = resources.getStringArray(R.array.orientacionSexualOpciones)
+        val tableLayout = binding.tablaOpciones
+
+        for (opcion in opciones) {
+            val checkBox = CheckBox(this)
+            checkBox.text = opcion
+            checkBox.setOnCheckedChangeListener { buttonView, isChecked ->
+                if (isChecked) {
+                    if (selectedOptions.size >= 3) {
+                        checkBox.isChecked = false
+                    } else {
+                        selectedOptions.add(opcion)
+                    }
+                } else {
+                    selectedOptions.remove(opcion)
+                }
+            }
+
+            val row = TableRow(this)
+            val params = TableRow.LayoutParams(
+                TableRow.LayoutParams.WRAP_CONTENT,
+                TableRow.LayoutParams.WRAP_CONTENT
+            )
+            params.setMargins(0, 0, 0, 20)
+            checkBox.layoutParams = params
+            row.addView(checkBox)
+            tableLayout.addView(row)
+        }
+
+        binding.mostrarGenero.setOnClickListener {
+
+        }
     }
+
 }
