0431225a39e278dddaf985dcb55048c81fa32a5c
joseManuel1234 <josemrodriguez@javeriana.edu.co>
2024-02-28 00:34:18 -0500

    feature:error chatActivity

--- app/src/main/java/com/example/loginactivity/SignUpActivity.kt
+++ app/src/main/java/com/example/loginactivity/SignUpActivity.kt
@@ -2,10 +2,25 @@ package com.example.loginactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import com.example.loginactivity.databinding.ActivitySignupBinding
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignupBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.iniciarSesion.setOnClickListener {
+            val intent = Intent(this, LogInActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.crearCuenta.setOnClickListener {
+            val intent = Intent(this, IngresarNumeroActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
