755c831aab08b9decb030725345501c33c01cb19
Luis Alejandro Vera Moreno <98617444+SouthThunder@users.noreply.github.com>
2024-02-28 01:18:50 -0500

    feature: added edit screen

--- app/src/main/java/com/example/loginactivity/EditActivity.kt
+++ app/src/main/java/com/example/loginactivity/EditActivity.kt
@@ -1,11 +1,20 @@
 package com.example.loginactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.loginactivity.databinding.ActivityEditBinding
 
 class EditActivity : AppCompatActivity() {
+    private lateinit var binding :ActivityEditBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit)
+        binding = ActivityEditBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.modifyPInfo.setOnClickListener {
+            var intent = Intent(this, SeleccionGeneroActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
