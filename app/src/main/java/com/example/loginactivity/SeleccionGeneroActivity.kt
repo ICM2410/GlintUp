0689f004ec6ff603c7ec1cf99f713b9f7ec870a8
joseManuel1234 <josemrodriguez@javeriana.edu.co>
2024-02-27 20:57:32 -0500

    feature:cosas añadidas

---
+++ app/src/main/java/com/example/loginactivity/SeleccionGeneroActivity.kt
@@ -0,0 +1,11 @@
+package com.example.loginactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SeleccionGeneroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seleccion_genero)
+    }
+}
